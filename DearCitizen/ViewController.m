2958afb794a2110e627cc22251861bf5f527fe47
JP Skowron <jpskowron@JP-Skowrons-MacBook-Pro.local>
2015-01-21 12:22:57 -0600

    first

--- DearCitizen/ViewController.m
+++ DearCitizen/ViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "ViewController.h"
+#import <CoreLocation/CoreLocation.h>
+#import <MapKit/MapKit.h>
 
-@interface ViewController ()
+@interface ViewController () <CLLocationManagerDelegate>
+@property (strong, nonatomic) IBOutlet UITextView *textView;
+@property CLLocationManager *locationManager;
 
 @end
 
@@ -16,12 +20,58 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    self.locationManager = [[CLLocationManager alloc]init];
+    self.locationManager.delegate = self;
+    [self.locationManager requestAlwaysAuthorization];
+
+}
+- (IBAction)startViolatingPrivacy:(id)sender {
+    [self.locationManager startUpdatingLocation];
+    self.textView.text = @"Locating you...";
+}
+- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error {
+    NSLog(@"%@", error);
+}
+-(void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations{
+    for (CLLocation *location in locations) {
+        if (location.horizontalAccuracy < 1000 && location.verticalAccuracy < 1000) {
+            self.textView.text = @"Location found. Reserve geo-coding...";
+            [self.locationManager stopUpdatingLocation];
+            [self reverseGeocode: location];
+            break;
+        }
+    }
+
+}
+-(void)reverseGeocode:(CLLocation *)location {
+    CLGeocoder *geocoder = [[CLGeocoder alloc]init];
+    [geocoder reverseGeocodeLocation:location completionHandler:^(NSArray *placemarks, NSError *error) {
+        CLPlacemark *placemark = placemarks.firstObject;
+
+        NSString *address ;
+        if (placemark.subThoroughfare && placemark.thoroughfare) {
+            address = [NSString stringWithFormat:@"%@ %@\n%@", placemark.subThoroughfare, placemark.thoroughfare, placemark.locality];
+        }else{
+            address = [[placemark.addressDictionary objectForKey:@"FormattedAddressLines"] componentsJoinedByString:@"\n"];
+        }
+        self.textView.text = address;
+
+        [self findJailNear: placemark.location];
+    }];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)findJailNear:(CLLocation *)location {
+    MKLocalSearchRequest *request = [[MKLocalSearchRequest alloc]init];
+    request.naturalLanguageQuery = @"Correctional";
+    request.region = MKCoordinateRegionMake(location.coordinate, MKCoordinateSpanMake(1, 1));
+
+    MKLocalSearch *search = [[MKLocalSearch alloc] initWithRequest:request];
+    [search startWithCompletionHandler:^(MKLocalSearchResponse *response, NSError *error) {
+        NSArray *mapItems = response.mapItems;
+        MKMapItem *mapItem = mapItems.firstObject;
+        self.textView.text = [NSString stringWithFormat:@"You're a guilty SOB! Report to: %@", mapItem.name];
+    }];
 }
 
 @end
